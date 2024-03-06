7f38ed095cad448d0e0a3845c90e8a004d8634c8
Esther Devadas <esmd258@gmail.com>
2024-03-06 11:05:03 -0500

    Router Setup

--- src/App.js
+++ src/App.js
@@ -1,16 +1,24 @@
+import { Routes, Route } from 'react-router-dom';
+import ContactPage from "./pages/ContactPage";
+import HomePage from "./pages/HomePage";
 import Header from "./components/Header";
+import Footer from './components/Footer';
 import './App.css';
 import CampsitesDirectoryPage from "./pages/CampsitesDirectoryPage";
-import Footer from './components/Footer';
+
 
 function App() {
-  return (
-    <div className="App">
-      <Header />
-      <CampsitesDirectoryPage />
-      <Footer />
-    </div>
-  );
+	return (
+		<div className="App">
+			<Header />
+			<Routes>
+				<Route path='/' element={<HomePage />} />
+				<Route path='contact' element={<ContactPage />} />
+				<Route path='directory' element={<CampsitesDirectoryPage />} />
+			</Routes>
+			<Footer />
+		</div>
+	);
 }
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,22 +1,26 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
+import { BrowserRouter } from "react-router-dom";
+import './index.css';
+import { store } from './app/store';
+import App from './App';
 import { Provider } from 'react-redux';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap-social/bootstrap-social.css';
 import 'font-awesome/css/font-awesome.css';
 import 'typeface-lobster';
 import 'typeface-open-sans';
-import { store } from './app/store';
-import App from './App';
-import './index.css';
+
 
 const container = document.getElementById('root');
 const root = createRoot(container);
 
 root.render(
-  <React.StrictMode>
-    <Provider store={store}>
-      <App />
-    </Provider>
-  </React.StrictMode>
+	<React.StrictMode>
+		<Provider store={store}>
+			<BrowserRouter>
+				<App />
+			</BrowserRouter>
+		</Provider>
+	</React.StrictMode>
 );
