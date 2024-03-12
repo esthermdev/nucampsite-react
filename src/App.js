0371bddcd0ce001521bfeb402a9eaa2781915bb1
Esther Devadas <esmd258@gmail.com>
2024-03-12 14:46:16 -0400

    to update

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import Footer from './components/Footer';
 import './App.css';
 import CampsitesDirectoryPage from "./pages/CampsitesDirectoryPage";
 import CampsiteDetailPage from "./pages/CampsiteDetailPage";
+import AboutPage from "./pages/AboutPage";
 
 
 function App() {
@@ -20,6 +21,7 @@ function App() {
 					path='directory/:campsiteId'
 					element={<CampsiteDetailPage />}
 				/>
+				<Route path='about' element={<AboutPage />} />
 			</Routes>
 			<Footer />
 		</div>
